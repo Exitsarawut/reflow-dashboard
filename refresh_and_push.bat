538bd8580bd318c4a6c544fbc0ac6e52f9385f99
Sarawut Musiket <SMK@gpv-asia.co.th>
2026-06-15 16:52:49 +0700

    update data

--- refresh_and_push.bat
+++ refresh_and_push.bat
@@ -1,6 +1,13 @@
 @echo off
 
+cd /d "C:\Users\SMK\OneDrive - GPV Group\Desktop\ReflowDashboard"
+
+echo Refreshing data...
 curl http://127.0.0.1:5000/refresh
+
+timeout /t 3
+
+echo Uploading to GitHub...
 
 git add .
 git commit -m "update data"
